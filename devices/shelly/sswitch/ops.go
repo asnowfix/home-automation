03665208523b08465451fbc2776d1e25707a4c36
Francois-Xavier 'FiX' Kowalski <fix.kowalski@gmail.com>
2023-12-08 15:34:50 +0100

    Try with per method output parser

--- devices/shelly/sswitch/ops.go
+++ devices/shelly/sswitch/ops.go
@@ -2,50 +2,29 @@ package sswitch
 
 import (
 	"devices/shelly"
-	"encoding/json"
-	"log"
 )
 
-// func init() {
-// 	shelly.RegisterMethod("Switch.GetConfig", GetConfigE, Configuration)
-// }
-
-func GetConfigE(d shelly.Device) (*Configuration, error) {
-
-	res, err := shelly.GetE(d, "Switch.GetConfig", shelly.MethodParams{})
-	if err != nil {
-		return nil, err
-	}
-
-	var c Configuration
-	err = json.NewDecoder(res.Body).Decode(&c)
-	if err != nil {
-		return nil, err
-	}
-	log.Default().Printf("Switch.GetConfig: %v\n", c)
-
-	return &c, nil
+func init() {
+	Initialize()
 }
 
-// func init() {
-// 	shelly.RegisterMethod("Switch.GetStatus", GetStatusE)
-// }
-
-func GetStatusE(d shelly.Device) (*Status, error) {
-	res, err := shelly.GetE(d, "Switch.Status", shelly.MethodParams{})
-	if err != nil {
-		return nil, err
-	}
-	var s Status
-	err = json.NewDecoder(res.Body).Decode(&s)
-	if err != nil {
-		return nil, err
-	}
-	log.Default().Printf("Status: %v\n", s)
-
-	return &s, nil
+func Initialize() {
+	shelly.ConfigureMethod("Switch.GetConfig", shelly.MethodConfiguration{
+		Allocate: func() any { return new(Configuration) },
+		Params: map[string]string{
+			"id": "0",
+		},
+	})
+	shelly.ConfigureMethod("Switch.GetStatus", shelly.MethodConfiguration{
+		Allocate: func() any { return new(Status) },
+		Params: map[string]string{
+			"id": "0",
+		},
+	})
+	shelly.ConfigureMethod("Switch.Toogle", shelly.MethodConfiguration{
+		Allocate: func() any { return new(Toogle) },
+		Params: map[string]string{
+			"id": "0",
+		},
+	})
 }
-
-// func ToggleE(d shelly.Device, s sswitch.Toggle) error {
-// 	return shelly.GetE(d, "Switch.Toogle")
-// }

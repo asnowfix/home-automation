9e59b42a809704da4dc857500f500eaaa94e3419
Francois-Xavier 'FiX' Kowalski <fix.kowalski@gmail.com>
2024-02-04 18:38:01 +0100

    MyHome server now annnounes itself over mDNS

--- myhome/main.go
+++ myhome/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/hashicorp/mdns"
 	mqtt "github.com/mochi-mqtt/server/v2"
 	"github.com/mochi-mqtt/server/v2/hooks/auth"
 	"github.com/mochi-mqtt/server/v2/listeners"
 )
 
+var Program string
+var Version string
 var Commit string
 
 func main() {
@@ -24,20 +28,36 @@ func main() {
 	}()
 
 	// Create the new MQTT Server.
-	server := mqtt.New(nil)
+	mqttServer := mqtt.New(nil)
 
 	// Allow all connections.
-	_ = server.AddHook(new(auth.AllowHook), nil)
+	_ = mqttServer.AddHook(new(auth.AllowHook), nil)
 
 	// Create a TCP listener on a standard port.
 	tcp := listeners.NewTCP("t1", ":1883", nil)
-	err := server.AddListener(tcp)
+	err := mqttServer.AddListener(tcp)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Publish over mDNS
+	host, _ := os.Hostname()
+	service := "_mqtt._tcp"
+	info := []string{
+		fmt.Sprintf("hostname=%v", host),
+		fmt.Sprintf("program=%v", Program),
+		fmt.Sprintf("version=%v", Version),
+		fmt.Sprintf("commit=%v", Commit),
+	}
+	mdnsService, _ := mdns.NewMDNSService(host, service, "", "", 1883, nil, info)
+	log.Default().Printf("publishing %v as %v over mDNS", info, service)
+
+	// Create the mDNS server, defer shutdown
+	mdnsServer, _ := mdns.NewServer(&mdns.Config{Zone: mdnsService})
+	defer mdnsServer.Shutdown()
+
 	go func() {
-		err := server.Serve()
+		err := mqttServer.Serve()
 		if err != nil {
 			log.Fatal(err)
 		}

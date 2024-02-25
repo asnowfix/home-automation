83b7cbf84ffae202d033a2f219be520a294f33b8
Francois-Xavier 'FiX' Kowalski <fix.kowalski@gmail.com>
2024-02-25 19:17:04 +0100

    Publish the MQTT broker via mDNS on the same interface as the one to reach the Home gateway

--- myhome/mqtt/server.go
+++ myhome/mqtt/server.go
@@ -3,8 +3,10 @@ package mqtt
 import (
 	"devices"
 	"log"
+	"net"
 	"os"
 
+	"github.com/jackpal/gateway"
 	mqtt "github.com/mochi-mqtt/server/v2"
 
 	"github.com/hashicorp/mdns"
@@ -12,7 +14,7 @@ import (
 	"github.com/mochi-mqtt/server/v2/listeners"
 )
 
-func MyHome(info []string) (*mdns.Server, error) {
+func MyHome(program string, info []string) (*mdns.Server, error) {
 	// Create the new MQTT Server.
 	mqttServer := mqtt.New(nil)
 
@@ -26,9 +28,24 @@ func MyHome(info []string) (*mdns.Server, error) {
 		log.Fatal(err)
 	}
 
-	host, _ := os.Hostname()
-	mdnsService, _ := mdns.NewMDNSService(host, devices.MqttService, "", "", 1883, nil, info)
-	log.Default().Printf("publishing %v as %v over mDNS", info, devices.MqttService)
+	addrs := make([]net.IP, 1)
+	addrs[0], err = gateway.DiscoverInterface()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	host, err := os.Hostname()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var instance string = program
+	if program == "" {
+		instance = host
+	}
+
+	log.Default().Printf("publishing %v as %v over mDNS via IP %v", info, devices.MqttService, addrs)
+	mdnsService, _ := mdns.NewMDNSService(instance, devices.MqttService, "" /*domain*/, "" /*host*/, 1883, addrs, info)
 
 	// Create the mDNS server, defer shutdown
 	mdnsServer, _ := mdns.NewServer(&mdns.Config{Zone: mdnsService})

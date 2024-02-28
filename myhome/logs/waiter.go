7160d7b31ab90bb9553653c89ba991b4ed317fd7
Francois-Xavier 'FiX' Kowalski <fix.kowalski@gmail.com>
2024-02-28 21:35:09 +0100

    Ensure MQTT message delivery (qos)

--- myhome/logs/waiter.go
+++ myhome/logs/waiter.go
@@ -10,17 +10,17 @@ func Waiter(tc chan string) {
 	for topic := range tc {
 		log.Default().Printf("logs.Waiter: topic: %v", topic)
 		if _, exists := topics[topic]; !exists {
-			log.Default().Printf("logs.Waiter: subscribing to topic: %v", topic)
+			log.Default().Printf("logs.Waiter: subscribing to %v", topic)
 			tc, err := devices.MqttSubscribe("logs.Waiter", topic, 0 /*qlen*/)
 			if err == nil {
-				log.Default().Printf("subscribed to %v", topic)
+				log.Default().Printf("logs.Waiter: subscribed to %v", topic)
 				topics[topic] = tc
 			} else {
-				log.Default().Printf("error subscribing to %v: %v", topic, err)
+				log.Default().Printf("logs.Waiter: error subscribing to %v (%v)", topic, err)
 			}
 			go func(t string, tc chan devices.MqttMessage) {
 				for msg := range tc {
-					log.Default().Printf("logs.Waiter: %v >>> %v", t, msg)
+					log.Default().Printf("logs.Waiter: %v: %v", t, string(msg.Payload))
 				}
 			}(topic, topics[topic])
 		} else {

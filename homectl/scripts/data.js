84715bc975af753a50d2ee75b02aeb2b1789a2f8
Francois-Xavier 'FiX' Kowalski <fix.kowalski@gmail.com>
2024-12-01 10:51:53 +0100

    store add-hoc scripts for backup

---
+++ homectl/scripts/data.js
@@ -0,0 +1,116 @@
+bluMotionSensorEventData = {
+    "encryption": false,
+    "BTHome_version": 2,
+    "pid": 105,
+    "battery": 98,
+    "illuminance": 174,
+    "motion": 1,
+    "rssi": -70,
+    "address": "e8:e0:7e:d0:f9:89",
+    "model": undefined
+}
+
+switch4IEventData = [{
+    "id": 1, "now": 1732463413.75878000259,
+    "info": {
+        "component": "input:1",
+        "id": 1,
+        "event": "toggle",
+        "state": true,
+        "ts": 1732463413.75999999046
+    }
+},
+{
+    "id": 1, "now": 1732463414.02464318275,
+    "info": {
+        "component": "input:1",
+        "id": 1,
+        "event": "toggle",
+        "state": false,
+        "ts": 1732463414.01999998092
+    }
+},
+
+{
+    "id": 0, "now": 1732463414.73114681243,
+    "info": {
+        "component": "input:0",
+        "id": 0,
+        "event": "toggle",
+        "state": true,
+        "ts": 1732463414.73000001907
+    }
+},
+{
+    "id": 0, "now": 1732463414.95820188522,
+    "info": {
+        "component": "input:0",
+        "id": 0,
+        "event": "toggle",
+        "state": false,
+        "ts": 1732463414.96000003814
+    }
+},
+{
+    "id": 2, "now": 1732463415.58661103248,
+    "info": {
+        "component": "input:2",
+        "id": 2,
+        "event": "toggle",
+        "state": true,
+        "ts": 1732463415.58999991416
+    }
+},
+{
+    "id": 2, "now": 1732463415.85527706146,
+    "info": {
+        "component": "input:2",
+        "id": 2,
+        "event": "toggle",
+        "state": false,
+        "ts": 1732463415.85999989509
+    }
+},
+{
+    "id": 3, "now": 1732463416.45232319831,
+    "info": {
+        "component": "input:3",
+        "id": 3,
+        "event": "toggle",
+        "state": true,
+        "ts": 1732463416.45000004768
+    }
+},
+{
+    "id": 3, "now": 1732463416.65927505493,
+    "info": {
+        "component": "input:3",
+        "id": 3,
+        "event": "toggle",
+        "state": false,
+        "ts": 1732463416.65999984741
+    }
+}]
+
+scriptReloadEventData = [{
+    "id": -1, "now": 1732473181.38573503494,
+    "info": {
+        "component": "sys",
+        "event": "component_removed",
+        "target": "script:3",
+        "restart_required": false,
+        "ts": 1732473181.38999986648,
+        "cfg_rev": 58
+    }
+}, {
+    "id": -1,
+    "now": 1732473209.48322010040,
+    "info": {
+        "component": "sys",
+        "event": "component_added",
+        "target": "script:3",
+        "restart_required": false,
+        "ts": 1732473209.48000001907,
+        "cfg_rev": 59
+    }
+}]

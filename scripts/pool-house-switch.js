01204ab21fa21bf90d5181abfac8f446b747c5e5
Francois-Xavier 'FiX' Kowalski <fix.kowalski@gmail.com>
2024-12-16 22:06:10 +0100

    Clean-up

--- scripts/pool-house-switch.js
+++ scripts/pool-house-switch.js
@@ -1,7 +1,6 @@
 Shelly.addEventHandler(function (eventData) {
     console.log("Handling event: ", eventData);
     try {
-        // main switch - pool-house-1 light
         if (eventData.id === 0 && eventData.info.event === "btn_up") {
             console.log("Toggle pool-house-1 light");
             MQTT.publish("shellyplus1-b8d61a85a8e0/rpc", JSON.stringify({"method":"Switch.Toggle", "params":{"id":0}}), 0, false);

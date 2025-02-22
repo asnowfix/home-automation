08242107f628735322d6ae7fd01b219d55b4209f
Francois-Xavier 'FiX' Kowalski <fix.kowalski@gmail.com>
2025-02-23 00:54:36 +0100

    Auto-update script as systemd

--- linux/debian/postinst.sh
+++ linux/debian/postinst.sh
@@ -2,23 +2,27 @@
 set -e
 
 SERVICE="myhome"
-SERVICE_FILE="${SERVICE}.service"
+SERVICE_FILES="${SERVICE}.service ${SERVICE}-update.service ${SERVICE}-update.timer"
 
 mkdir -p /var/lib/$SERVICE
 
 # Check if the script is being run during package installation
 if [ "$1" = "configure" ]; then
-    # Copy the service file to the systemd directory
-    cp /usr/share/$SERVICE/$SERVICE_FILE /etc/systemd/system/
+    # Link the service files to the systemd directory
+    for f in $SERVICE_FILES; do
+        (cd /etc/systemd/system && ln -sf /lib/systemd/system/$f .)
+    done
 
     # Reload systemd to recognize the new service
     systemctl daemon-reload
 
     # Enable the service to start on boot
-    systemctl enable $SERVICE_FILE
+    systemctl enable $SERVICE.service
+    systemctl enable $SERVICE-update.timer
 
-    # Start the service immediately
-    systemctl start $SERVICE_FILE
+    # (Re)Start the service immediately
+    systemctl restart $SERVICE.service
+    systemctl restart $SERVICE-update.timer
 fi
 
 # Exit successfully

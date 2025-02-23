0014eb7926f4596563b721835842c4053557fd7e
Francois-Xavier 'FiX' Kowalski <fix.kowalski@gmail.com>
2025-02-23 14:27:51 +0100

    Refresh in-memory device(s) from strorage

--- data-shelly.js
+++ data-shelly.js
@@ -1,4 +1,4 @@
-{
+result = {
   "ble": {
     "enable": true,
     "observer": {
@@ -117,5 +117,90 @@
     "enable": false,
     "server": null,
     "ssl_ca": "ca.pem"
+  }
+}
+
+
+results_sys_get_config_1 = {
+  "id": 0,
+  "src": "shelly1minig3-543204522cb4",
+  "dst": "__debug_bin1505241838-palmbeach-2543369_shelly1minig3-543204522cb4",
+  "result": {
+    "device": {
+      "name": "front-door-light",
+      "mac": "543204522CB4",
+      "fw_id": "20241011-114456/1.4.4-g6d2a586",
+      "discoverable": true,
+      "eco_mode": false
+    },
+    "location": {
+      "tz": "Europe/Paris",
+      "lat": 43.6611,
+      "lon": 6.9808
+    },
+    "debug": {
+      "level": 2,
+      "file_level": null,
+      "mqtt": {
+        "enable": false
+      },
+      "websocket": {
+        "enable": true
+      },
+      "udp": {
+        "addr": null
+      }
+    },
+    "ui_data": {},
+    "rpc_udp": {
+      "dst_addr": null,
+      "listen_port": null
+    },
+    "sntp": {
+      "server": "time.google.com"
+    },
+    "cfg_rev": 52
+  }
+}
+
+results_sys_get_config_2 = {
+  "id": 0,
+  "src": "shelly1minig3-543204522cb4",
+  "dst": "__debug_bin331115640-palmbeach-2545289_shelly1minig3-543204522cb4",
+  "result": {
+    "device": {
+      "name": "front-door-light",
+      "mac": "543204522CB4",
+      "fw_id": "20241011-114456/1.4.4-g6d2a586",
+      "discoverable": true,
+      "eco_mode": false
+    },
+    "location": {
+      "tz": "Europe/Paris",
+      "lat": 43.6611,
+      "lon": 6.9808
+    },
+    "debug": {
+      "level": 2,
+      "file_level": null,
+      "mqtt": {
+        "enable": false
+      },
+      "websocket": {
+        "enable": true
+      },
+      "udp": {
+        "addr": null
+      }
+    },
+    "ui_data": {},
+    "rpc_udp": {
+      "dst_addr": null,
+      "listen_port": null
+    },
+    "sntp": {
+      "server": "time.google.com"
+    },
+    "cfg_rev": 52
   }
 }

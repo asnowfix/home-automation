674925ff7382f82ae956ba1f9e2778367720b58f
Francois-Xavier 'FiX' KOWALSKI <fix.kowalski@gmail.com>
2025-05-23 18:28:18 +0200

    Update binary artifact only (WIP)

--- linux/debian/update.sh
+++ linux/debian/update.sh
@@ -3,8 +3,8 @@ set -ex
 REPO=asnowfix/home-automation
 ARCH=$(dpkg --print-architecture)
 TMPDIR=$(mktemp -d)
-PACKAGE="myhome_.deb"
-URL=$(curl -L -s https://api.github.com/repos/${REPO}/releases/latest | jq -r --arg suffix "${ARCH}.deb" '.assets | .[] | select(.name | endswith($suffix)) | .browser_download_url')
+PACKAGE="myhome_${ARCH}.deb"
+URL=$(curl -L -s https://api.github.com/repos/${REPO}/releases/latest | jq -r --arg suffix "${PACKAGE}" '.assets | .[] | select(.name | endswith($suffix)) | .browser_download_url')
 curl -L -o $TMPDIR/$PACKAGE $URL
 sudo dpkg -i "$TMPDIR/$PACKAGE"
 rm -rf "$TMPDIR"

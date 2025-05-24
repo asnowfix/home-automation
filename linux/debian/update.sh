a8ee5d7a255bd98b7e73db62b88c8e417c4d5c88
Francois-Xavier 'FiX' KOWALSKI <fix.kowalski@gmail.com>
2025-05-24 23:48:43 +0200

    fix: update package release workflow and debian install script with version handling and systemd integration

--- linux/debian/update.sh
+++ linux/debian/update.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -ex
 REPO=asnowfix/home-automation
-ARCH=$(dpkg --print-architecture)
+ARCH=$(dpkg --print-architecture | sed 's/.*-//')
+
 TMPDIR=$(mktemp -d)
-PACKAGE="myhome_${ARCH}.deb"
-URL=$(curl -L -s https://api.github.com/repos/${REPO}/releases/latest | jq -r --arg suffix "${PACKAGE}" '.assets | .[] | select(.name | endswith($suffix)) | .browser_download_url')
-curl -L -o $TMPDIR/$PACKAGE $URL
-sudo dpkg -i "$TMPDIR/$PACKAGE"
+pushd $TMPDIR
+SUFFIX="${ARCH}.deb"
+URL=$(curl -L -s https://api.github.com/repos/${REPO}/releases/latest | jq -r --arg suffix "${SUFFIX}" '.assets | .[] | select(.name | endswith($suffix)) | .browser_download_url')
+curl -L -o pkg.deb $URL
+sudo dpkg -i pkg.deb
+popd
 rm -rf "$TMPDIR"
+
+sudo apt-get install -f
+sudo systemctl daemon-reload
+sudo systemctl enable myhome
+sudo systemctl restart myhome
+sudo systemctl status myhome

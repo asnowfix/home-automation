2b699936e3672525ebe7d53b6e051a13ebe05dfd
Francois-Xavier 'FiX' KOWALSKI <fix.kowalski@gmail.com>
2025-05-25 10:37:15 +0200

    fix: improve service management and GitHub token handling in debian package scripts

--- linux/debian/postinst.sh
+++ linux/debian/postinst.sh
@@ -8,17 +8,12 @@ mkdir -p /var/lib/$SERVICE
 
 # Check if the script is being run during package installation
 if [ "$1" = "configure" ]; then
-    # Link the service files to the systemd directory
-    for f in $SERVICE_FILES; do
-        (cd /etc/systemd/system && ln -sf /lib/systemd/system/$f .)
-    done
-
     # Reload systemd to recognize the new service
     systemctl daemon-reload
 
     # Enable the service to start on boot
-    systemctl enable $SERVICE.service
-    systemctl enable $SERVICE-update.timer
+    systemctl reenable $SERVICE.service
+    systemctl reenable $SERVICE-update.timer
 
     # (Re)Start the service immediately
     systemctl restart $SERVICE.service
--- linux/debian/update.sh
+++ linux/debian/update.sh
@@ -8,10 +8,13 @@ SUFFIX="${ARCH}.deb"
 GITHUB_CURL_ARGS="-Ls -H Accept:application/vnd.github.v3.raw"
 if [ -n "$GITHUB_TOKEN" ]; then
     GITHUB_CURL_ARGS="${GITHUB_CURL_ARGS} --oauth2-bearer $GITHUB_TOKEN"
+    URL=$(curl -s "https://api.github.com/repos/${ORG}/${REPO}/releases?per_page=1" $GITHUB_CURL_ARGS | 
+        jq -r --arg suffix $SUFFIX '.[0].assets| .[] | select(.name | endswith($suffix)) | .browser_download_url')
+else
+    URL=$(curl $GITHUB_CURL_ARGS "https://api.github.com/repos/${ORG}/${REPO}/releases/latest" | 
+        jq -r --arg suffix $SUFFIX '.assets[] | select(.name | endswith($suffix)) | .browser_download_url')
 fi
 
-URL=$(curl $GITHUB_CURL_ARGS "https://api.github.com/repos/${ORG}/${REPO}/releases/latest" | 
-    jq -r --arg suffix $SUFFIX '.assets[] | select(.name | endswith($suffix)) | .browser_download_url')
 echo "Fetching: $URL" >&2
 
 TMPDIR=$(mktemp -d)
@@ -19,15 +22,9 @@ pushd $TMPDIR
 curl $GITHUB_CURL_ARGS -H 'Accept: application/octet-stream' -O "$URL"
 echo "Installing: $(ls *$SUFFIX)" >&2
 sudo dpkg -i *$SUFFIX
-apt-get install -f
+sudo apt-get install -f
 popd
 rm -rf "$TMPDIR"
-
-echo "Restarting service..." >&2
-systemctl daemon-reload
-systemctl enable myhome
-systemctl restart myhome
-systemctl status myhome
 
 echo "Update completed successfully." >&2
 exit 0

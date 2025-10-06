c8e06589c2d0944a20771a5d85672d661731e348
Francois-Xavier 'FiX' KOWALSKI <fix.kowalski@gmail.com>
2025-10-06 08:41:40 +0200

    feat: add JSON and YAML output formatting for device job listings

--- myhome/ctl/shelly/jobs/show.go
+++ myhome/ctl/shelly/jobs/show.go
@@ -2,6 +2,7 @@ package jobs
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"hlog"
 	"myhome"
@@ -11,6 +12,7 @@ import (
 
 	"github.com/go-logr/logr"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
 
 	"pkg/devices"
 	"pkg/shelly"
@@ -34,12 +36,40 @@ func showOneDeviceJobs(ctx context.Context, log logr.Logger, via types.Channel, 
 	}
 	out, err := schedule.ShowJobs(ctx, log, via, sd)
 	if err != nil {
-		log.Error(err, "Unable to set Scheduled JobSpec: %v", err)
+		log.Error(err, "Unable to get scheduled jobs: %v", err)
 		return nil, err
 	}
 
 	jobs := out.(*schedule.Scheduled)
-	log.Info("Scheduled", "jobs", jobs)
+	
+	// Prepare complete output as a single string to avoid interleaving
+	var output string
+	deviceHeader := fmt.Sprintf("%s (%s)", sd.Name(), sd.Id())
+	
+	if options.Flags.Json {
+		// For JSON, include device info in the output
+		jsonOutput := map[string]interface{}{
+			"device": deviceHeader,
+			"id":     sd.Id(),
+			"name":   sd.Name(),
+			"jobs":   jobs,
+		}
+		s, err := json.MarshalIndent(jsonOutput, "", "  ")
+		if err != nil {
+			return nil, err
+		}
+		output = string(s)
+	} else {
+		// For YAML, build complete output with header and jobs
+		s, err := yaml.Marshal(jobs)
+		if err != nil {
+			return nil, err
+		}
+		output = fmt.Sprintf("# %s\n%s", deviceHeader, string(s))
+	}
+	
+	// Print complete output atomically
+	fmt.Print(output)
 
 	return jobs, nil
 }

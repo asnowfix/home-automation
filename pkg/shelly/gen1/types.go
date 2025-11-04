417755cc2163cc10663f5dc9265b354d478c27f2
Francois-Xavier 'FiX' KOWALSKI <fix.kowalski@gmail.com>
2025-11-04 19:44:43 +0100

    refactor: flatten sensor data structure in Device type
    - Embedded Sensor struct directly into Device to simplify data access
    - Removed IsHTSensor() and IsFloodSensor() helper methods in favor of nil checks
    - Updated MQTT publishing logic to check individual sensor fields instead of nested Sensor object

--- pkg/shelly/gen1/types.go
+++ pkg/shelly/gen1/types.go
@@ -4,15 +4,12 @@ import (
 	"net"
 )
 
+// Sensor contains sensor data from Gen1 devices
 type Sensor struct {
 	Temperature *float32 `schema:"temp" json:"temperature,omitempty"`
-
-	// H&T specific (optional)
-	Humidity *uint `schema:"hum" json:"humidity,omitempty"`
-
-	// Flood specific (optional)
-	Flood          *uint32  `schema:"flood" json:"flood,omitempty"`
-	BatteryVoltage *float32 `schema:"batV" json:"battery_voltage,omitempty"`
+	Humidity    *uint    `schema:"hum" json:"humidity,omitempty"`
+	Flood       *uint32  `schema:"flood" json:"flood,omitempty"`
+	BatV        *float32 `schema:"batV" json:"battery_voltage,omitempty"`
 }
 
 // The gen1.Device struct is used to represent a Gen1 device
@@ -21,20 +18,10 @@ type Sensor struct {
 // <https://shelly-api-docs.shelly.cloud/gen1/#shelly-h-amp-t>
 type Device struct {
 	// Common fields
-	Id           string  `schema:"id" json:"id"`
-	Ip           net.IP  `json:"ip"`
-	FirmwareDate string  `json:"fw_date,omitempty"`
-	FirmwareId   string  `json:"fw_id,omitempty"`
-	Model        string  `json:"model,omitempty"`
-	Sensor       *Sensor `json:"sensor,omitempty"`
-}
-
-// IsHTSensor returns true if this is a Humidity & Temperature sensor
-func (d *Device) IsHTSensor() bool {
-	return d.Sensor != nil && d.Sensor.Humidity != nil
-}
-
-// IsFloodSensor returns true if this is a Flood sensor
-func (d *Device) IsFloodSensor() bool {
-	return d.Sensor != nil && d.Sensor.Flood != nil
+	Id           string `schema:"id" json:"id"`
+	Ip           net.IP `json:"ip"`
+	FirmwareDate string `json:"fw_date,omitempty"`
+	FirmwareId   string `json:"fw_id,omitempty"`
+	Model        string `json:"model,omitempty"`
+	Sensor       `json:"sensor,omitempty"`
 }
